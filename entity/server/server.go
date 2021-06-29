ecd69ada6b5599fb49f95f5fce1bdd4876d8bf3d
NhoanNhoan <kinhn11@gmail.com>
2021-06-29 21:37:51 +0700

    add server entity

--- entity/server/server.go
+++ entity/server/server.go
@@ -1,1 +1,12 @@
 package server
+
+type Server struct {
+	ID           string `gorm:"primaryKey"`
+	SerialNumber string
+	DC
+	Rack
+	UStart RackUnit
+	UEnd   RackUnit
+	WorkingState
+	ActiveState
+}
