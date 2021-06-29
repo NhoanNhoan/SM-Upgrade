432e7dbe8b74eb5c4efc8cc65384fc62485e84d1
NhoanNhoan <kinhn11@gmail.com>
2021-06-29 21:32:43 +0700

    add hardware type

--- entity/server/unit.go
+++ entity/server/unit.go
@@ -1,7 +1,7 @@
 package server
 
 type Unit struct {
-	ID string	`"gorm:primaryKey"`
+	ID          string `"gorm:primaryKey"`
 	Description string
 }
 
@@ -11,3 +11,12 @@ type RackUnit = Unit
 type PortType = Unit
 type WorkingState = Unit
 type ActiveState = Unit
+
+// Hardware
+type PSU = Unit
+type Chassis = Unit
+type CPU = Unit
+type Disk = Unit
+type NIC = Unit
+type RAID = Unit
+type RAM = Unit
