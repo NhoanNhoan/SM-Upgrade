6c44a91edf6344d8eeb88677f1c7a82304e7659e
NhoanNhoan <kinhn11@gmail.com>
2021-06-30 13:59:19 +0700

    tag type another file

--- entity/tag/tag.go
+++ entity/tag/tag.go
@@ -1,12 +1,5 @@
 package tag
 
-type TagType = string
-
-const (
-	IPAnyCastTag TagType = "IP AnyCast"
-	ServerTag    TagType = "Server Tag"
-)
-
 type Tag struct {
 	ID    string
 	Title string
---
+++ entity/tag/tag_type.go
@@ -0,0 +1,6 @@
+package tag
+
+type TagType struct {
+	Id string
+	Des string
+}
