8b2d26197d5c11e7fcbc19660aee79b7df20f221
NhoanNhoan <kinhn11@gmail.com>
2021-07-19 14:09:11 +0700

    remove ustart, uend

--- entity/server/server.go
+++ entity/server/server.go
@@ -3,10 +3,8 @@ package server
 type Server struct {
 	ID           string `gorm:"primaryKey"`
 	SerialNumber string
-	DC	`gorm:"foreignKey:ID;references:ID"`
-	Rack `gorm:"foreignKey:ID;references:ID"`
-	UStart RackUnit `gorm:"foreignKey:ID;references:ID"`
-	UEnd   RackUnit `gorm:"foreignKey:ID;references:ID"`
+	DC           `gorm:"foreignKey:ID;references:ID"`
+	Rack         `gorm:"foreignKey:ID;references:ID"`
 	WorkingState `gorm:"foreignKey:ID;references:ID"`
-	ActiveState `gorm:"foreignKey:ID;references:ID"`
+	ActiveState  `gorm:"foreignKey:ID;references:ID"`
 }
